24928ee0c694172cbe1c6aad9ee8fce824d72f82
leonandsmiggles <leonmac@stanford.edu>
2024-06-02 16:10:48 -0700

    Event list, and other minor changes to make it work on my laptop

--- components/Events.js
+++ components/Events.js
@@ -1,77 +1,218 @@
-import React, { useState, useCallback } from 'react';
-import { 
+import React, { useState, useCallback, useEffect } from "react";
+import {
   ActivityIndicator,
-  Alert,
   Dimensions,
-  Image,
-  Pressable,
-  StyleSheet,
+  ScrollView,
   Text,
-  TextInput,
-  View
+  TouchableOpacity,
+  View,
+  StyleSheet,
 } from "react-native";
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { LinearGradient } from 'expo-linear-gradient';
-import BottomBar from './BottomBar';
-import { supabase } from '../supabase';
+import { LinearGradient } from "expo-linear-gradient";
+import BottomBar from "./BottomBar";
+import EventCard from "./EventCard"; // Assuming EventCard is the component to render each event
+import { supabase } from "../supabase";
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const Events = ({ navigation }) => {
-    const [user_id, setUserID]= useState("");
-    const [mounting, setMounting] = useState(false);
+  const [user_id, setUserID] = useState("");
+  const [mounting, setMounting] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("upcoming");
+  const [events, setEvents] = useState([]);
 
-    useFocusEffect(
-        useCallback(() => {
-            async function fetchData() {
-                setMounting(true);
+  useFocusEffect(
+    useCallback(() => {
+      async function fetchData() {
+        setMounting(true);
+        try {
+          const auth_response = await supabase.auth.getUser();
+          if (auth_response.error) throw auth_response.error;
 
-                supabase.auth.getUser()
-                .then((auth_response) => {
-                    if (auth_response.error) throw auth_response.error;
+          const id = auth_response.data.user.id;
+          setUserID(id);
+        } catch (auth_error) {
+          console.log(auth_error);
+        } finally {
+          setMounting(false);
+        }
+      }
+      fetchData();
+    }, [])
+  );
 
-                    const id = auth_response.data.user.id;
-                    setUserID(id);
-                    setMounting(false);
-                }).catch((auth_error) => {
-                    console.log(auth_error);
-                })
-            }
-            fetchData();
-        }, [])
-    );
-
-    const [fontsLoaded] = useFonts({
-        "Dongle-Bold": require("../assets/fonts/Dongle-Bold.ttf"),
-        "Dongle-Regular": require("../assets/fonts/Dongle-Regular.ttf"),
-        "Dongle-Light": require("../assets/fonts/Dongle-Light.ttf"),
-    });
-
-    if (!fontsLoaded) {
-        return (
-            <LinearGradient colors={['#A8D0F5', '#D0B4F4']} style={styles.eventsContainer}>
-                <ActivityIndicator size="large" />
-            </LinearGradient>
-        );
+  useEffect(() => {
+    if (user_id) {
+      fetchEvents();
     }
-    
-    return(
-        <LinearGradient colors={['#A8D0F5', '#D0B4F4']} style={styles.eventsContainer}>
-            <Text>Events</Text>
-            <BottomBar navigation={navigation} />
-        </LinearGradient>
+  }, [user_id, selectedTab]);
+
+  const fetchEvents = async () => {
+    setMounting(true);
+    try {
+      let query = supabase
+        .from("events") // Assuming 'events' is the table name
+        .select("*");
+
+      const now = new Date().toISOString();
+
+      if (selectedTab === "upcoming") {
+        query = query.contains("participants", [user_id]).gt("end_time", now);
+      } else if (selectedTab === "my") {
+        query = query.eq("owner", user_id).gt("end_time", now);
+      } else if (selectedTab === "past") {
+        query = query
+          .or(`participants.cs.{${user_id}},owner.eq.${user_id}`)
+          .lt("end_time", now);
+      }
+      const { data, error } = await query;
+      if (error) throw error;
+      setEvents(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setMounting(false);
+    }
+  };
+
+  const [fontsLoaded] = useFonts({
+    "Dongle-Bold": require("../assets/fonts/Dongle-Bold.ttf"),
+    "Dongle-Regular": require("../assets/fonts/Dongle-Regular.ttf"),
+    "Dongle-Light": require("../assets/fonts/Dongle-Light.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <LinearGradient
+        colors={["#A8D0F5", "#D0B4F4"]}
+        style={styles.eventsContainer}
+      >
+        <ActivityIndicator size="large" />
+      </LinearGradient>
     );
-}
+  }
+
+  return (
+    <LinearGradient colors={["#A8D0F5", "#D0B4F4"]} style={styles.BigContainer}>
+      <Text style={styles.title}>EVENT CALENDAR</Text>
+      <View style={styles.tabsContainer}>
+        <TouchableOpacity
+          onPress={() => setSelectedTab("upcoming")}
+          style={[
+            styles.tabBox,
+            selectedTab === "upcoming" && styles.activeTabBox,
+          ]}
+        >
+          <Text
+            style={
+              selectedTab === "upcoming" ? styles.activeTab : styles.inactiveTab
+            }
+          >
+            Upcoming
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setSelectedTab("my")}
+          style={[styles.tabBox, selectedTab === "my" && styles.activeTabBox]}
+        >
+          <Text
+            style={selectedTab === "my" ? styles.activeTab : styles.inactiveTab}
+          >
+            My Events
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setSelectedTab("past")}
+          style={[styles.tabBox, selectedTab === "past" && styles.activeTabBox]}
+        >
+          <Text
+            style={
+              selectedTab === "past" ? styles.activeTab : styles.inactiveTab
+            }
+          >
+            History{" "}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {mounting ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <ScrollView contentContainerStyle={styles.eventsList}>
+          {events.map((event) => (
+            <View key={event.id} style={styles.eventCardContainer}>
+              <EventCard event={event} navigation={navigation} />
+            </View>
+          ))}
+        </ScrollView>
+      )}
+      <BottomBar navigation={navigation} />
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
-    eventsContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        paddingBottom: screenHeight * 0.1,
-    }
+  eventsContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  BigContainer: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: screenHeight * 0.1,
+    width: screenWidth,
+    height: screenHeight,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  tabsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: screenWidth * 0.9,
+    marginBottom: 20,
+  },
+  tabBox: {
+    backgroundColor: "#fff",
+    opacity: 0.7,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    elevation: 3, // Adds shadow for Android
+    shadowColor: "#000", // Adds shadow for iOS
+    shadowOffset: { width: 0, height: 2 }, // Adds shadow for iOS
+    shadowOpacity: 0.3, // Adds shadow for iOS
+    shadowRadius: 3, // Adds shadow for iOS
+    marginHorizontal: 5,
+  },
+  activeTabBox: {
+    backgroundColor: "#ddd",
+  },
+  activeTab: {
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  inactiveTab: {
+    color: "#888",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  eventsList: {
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  eventCardContainer: {
+    width: screenWidth * 0.9,
+    alignItems: "center",
+    marginBottom: 10,
+  },
 });
 
 export default Events;
--- components/UserPrompt1.js
+++ components/UserPrompt1.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { 
+import React, { useCallback, useState } from "react";
+import {
   ActivityIndicator,
   Alert,
   BackHandler,
@@ -8,295 +8,313 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View
-} from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { LinearGradient } from 'expo-linear-gradient';
-import { supabase } from '../supabase';
-import Prompts from './Prompts.js';
+  View,
+} from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
+import { supabase } from "../supabase";
+import Prompts from "./prompts.js";
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const UserPrompt1 = ({ navigation }) => {
-    const [user_id, setUserID]= useState("");
-    const [prompt1, setPrompt1] = useState("");
-    const [prompt2, setPrompt2] = useState("");
-    const [response1, setResponse1] = useState("");
-    const [finished_sign_up, setFinishedSignUp] = useState(false);
-    const [mounting, setMounting] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [user_id, setUserID] = useState("");
+  const [prompt1, setPrompt1] = useState("");
+  const [prompt2, setPrompt2] = useState("");
+  const [response1, setResponse1] = useState("");
+  const [finished_sign_up, setFinishedSignUp] = useState(false);
+  const [mounting, setMounting] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    useFocusEffect(
-        useCallback(() => {
-            async function fetchData() {
-                setMounting(true);
+  useFocusEffect(
+    useCallback(() => {
+      async function fetchData() {
+        setMounting(true);
 
-                supabase.auth.getUser()
-                .then((auth_response) => {
-                    if (auth_response.error) throw auth_response.error;
+        supabase.auth
+          .getUser()
+          .then((auth_response) => {
+            if (auth_response.error) throw auth_response.error;
 
-                    const id = auth_response.data.user.id;
-                    setUserID(id);
+            const id = auth_response.data.user.id;
+            setUserID(id);
 
-                    supabase
-                    .from('users')
-                    .select('prompt1, prompt2, response1, finished_sign_up')
-                    .eq('user_id', id)
-                    .then((response) => {
-                        if (response.error) throw response.error;
-                        
-                        const result = response.data[0];
-                        const p1 = result.prompt1;
-                        const p2 = result.prompt2;
-                        const r1 = result.response1;
-                        const fsu = result.finished_sign_up;
-                        
-                        if (p1) {
-                            setPrompt1(p1);
-                        }
-                        if (p2) {
-                            setPrompt2(p2);
-                        }
-                        if (r1) {
-                            setResponse1(r1);
-                        }
-                        setFinishedSignUp(fsu);
-                        setMounting(false);
-                    }).catch((error) => {
-                        console.log(error);
-                    });
-                }).catch((auth_error) => {
-                    console.log(auth_error);
-                })
-            }
-            fetchData();
-        }, [])
-    );
+            supabase
+              .from("users")
+              .select("prompt1, prompt2, response1, finished_sign_up")
+              .eq("user_id", id)
+              .then((response) => {
+                if (response.error) throw response.error;
 
-    const handleBack = async () => {
-        if (finished_sign_up) {
-            navigation.navigate("Account", { user_id: user_id });
-        }
-        else {
-            navigation.navigate("UserPhoto")
-        }
-    }
+                const result = response.data[0];
+                const p1 = result.prompt1;
+                const p2 = result.prompt2;
+                const r1 = result.response1;
+                const fsu = result.finished_sign_up;
 
-    const handlePromptRefresh = async () => {
-        let prompt = Prompts[Math.floor(Math.random() * Prompts.length)];
-        while (prompt === prompt1 || prompt === prompt2) {
-            prompt = Prompts[Math.floor(Math.random() * Prompts.length)];
-        }
-        setPrompt1(prompt);
-    }
-
-    const handleClick = async () => {
-        if (!prompt1) {
-            Alert.alert("Uhoh", "Please select a prompt!");
-            return;
-        }
-        if (!response1) {
-            Alert.alert("Uhoh", "Please write a response!");
-            return;
-        }
-
-        setLoading(true);
-        
-        const { error } = await supabase
-        .from('users')
-        .update({ prompt1: prompt1, response1: response1 })
-        .eq('user_id', user_id);
-        
-        setLoading(false);
-
-        if (error) {
-            Alert.alert("Uhoh", error.message);
-            return;
-        }
-
-        if (finished_sign_up) {
-            Alert.alert("Updated Successfully");
-            navigation.navigate("Account", { user_id: user_id });
-        }
-        else {
-            navigation.navigate("UserPrompt2")
-        }
-    }
-
-    const [fontsLoaded] = useFonts({
-        "Dongle-Bold": require("../assets/fonts/Dongle-Bold.ttf"),
-        "Dongle-Regular": require("../assets/fonts/Dongle-Regular.ttf"),
-        "Dongle-Light": require("../assets/fonts/Dongle-Light.ttf"),
-    });
-
-    if (!fontsLoaded || mounting) {
-        return (
-            <LinearGradient colors={['#A8D0F5', '#D0B4F4']} style={styles.namesContainer}>
-                <ActivityIndicator size="large" />
-            </LinearGradient>
-        );
-    }
-
-    return (
-        <LinearGradient colors={['#A8D0F5', '#D0B4F4']} style={styles.namesContainer}>
-            <Text style={styles.titleText}>Select your first prompt!</Text>
-            <View style={styles.promptContainer}>
-                <Text style={styles.promptText} multiline={true}>{prompt1 ? prompt1: "Prompt #1"}</Text>
-                <Pressable onPress={handlePromptRefresh} style={styles.refresh}>
-                    <FontAwesome
-                        name='refresh'
-                        size={screenWidth*0.1}
-                        color='#77678C'
-                    />
-                    <Text style={styles.refreshText}>New Prompt</Text>
-                </Pressable>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={setResponse1}
-                    value={response1}
-                    placeholder="Type your response here..."
-                    placeholderTextColor={'#888888'}
-                    maxLength={100}
-                    multiline={true}
-                />
-                <View style={styles.charactersLeftContainer}>
-                    <Text style={[styles.charactersLeft, 100 - response1.length < 20 && {color: '#e74c3c'}]}>Characters left: {100 - response1.length}</Text>
-                </View>
-            </View>
-            <View style={styles.lowerContainer}>
-                {
-                    loading ? 
-                    <ActivityIndicator style={styles.loading} /> :
-                    <View style={styles.buttonContainer}>
-                        <Pressable style={styles.backButton} onPress={handleBack}>
-                            <Text style={styles.buttonText}>Back</Text>
-                        </Pressable>
-                        <Pressable style={styles.button} onPress={handleClick}>
-                            <Text style={styles.buttonText}>{finished_sign_up ? "Save" : "Continue"}</Text>
-                        </Pressable>
-                    </View>
+                if (p1) {
+                  setPrompt1(p1);
                 }
-            </View>
-        </LinearGradient>
-      )
-}
+                if (p2) {
+                  setPrompt2(p2);
+                }
+                if (r1) {
+                  setResponse1(r1);
+                }
+                setFinishedSignUp(fsu);
+                setMounting(false);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((auth_error) => {
+            console.log(auth_error);
+          });
+      }
+      fetchData();
+    }, [])
+  );
+
+  const handleBack = async () => {
+    if (finished_sign_up) {
+      navigation.navigate("Account", { user_id: user_id });
+    } else {
+      navigation.navigate("UserPhoto");
+    }
+  };
+
+  const handlePromptRefresh = async () => {
+    let prompt = Prompts[Math.floor(Math.random() * Prompts.length)];
+    while (prompt === prompt1 || prompt === prompt2) {
+      prompt = Prompts[Math.floor(Math.random() * Prompts.length)];
+    }
+    setPrompt1(prompt);
+  };
+
+  const handleClick = async () => {
+    if (!prompt1) {
+      Alert.alert("Uhoh", "Please select a prompt!");
+      return;
+    }
+    if (!response1) {
+      Alert.alert("Uhoh", "Please write a response!");
+      return;
+    }
+
+    setLoading(true);
+
+    const { error } = await supabase
+      .from("users")
+      .update({ prompt1: prompt1, response1: response1 })
+      .eq("user_id", user_id);
+
+    setLoading(false);
+
+    if (error) {
+      Alert.alert("Uhoh", error.message);
+      return;
+    }
+
+    if (finished_sign_up) {
+      Alert.alert("Updated Successfully");
+      navigation.navigate("Account", { user_id: user_id });
+    } else {
+      navigation.navigate("UserPrompt2");
+    }
+  };
+
+  const [fontsLoaded] = useFonts({
+    "Dongle-Bold": require("../assets/fonts/Dongle-Bold.ttf"),
+    "Dongle-Regular": require("../assets/fonts/Dongle-Regular.ttf"),
+    "Dongle-Light": require("../assets/fonts/Dongle-Light.ttf"),
+  });
+
+  if (!fontsLoaded || mounting) {
+    return (
+      <LinearGradient
+        colors={["#A8D0F5", "#D0B4F4"]}
+        style={styles.namesContainer}
+      >
+        <ActivityIndicator size="large" />
+      </LinearGradient>
+    );
+  }
+
+  return (
+    <LinearGradient
+      colors={["#A8D0F5", "#D0B4F4"]}
+      style={styles.namesContainer}
+    >
+      <Text style={styles.titleText}>Select your first prompt!</Text>
+      <View style={styles.promptContainer}>
+        <Text style={styles.promptText} multiline={true}>
+          {prompt1 ? prompt1 : "Prompt #1"}
+        </Text>
+        <Pressable onPress={handlePromptRefresh} style={styles.refresh}>
+          <FontAwesome
+            name="refresh"
+            size={screenWidth * 0.1}
+            color="#77678C"
+          />
+          <Text style={styles.refreshText}>New Prompt</Text>
+        </Pressable>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setResponse1}
+          value={response1}
+          placeholder="Type your response here..."
+          placeholderTextColor={"#888888"}
+          maxLength={100}
+          multiline={true}
+        />
+        <View style={styles.charactersLeftContainer}>
+          <Text
+            style={[
+              styles.charactersLeft,
+              100 - response1.length < 20 && { color: "#e74c3c" },
+            ]}
+          >
+            Characters left: {100 - response1.length}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.lowerContainer}>
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.backButton} onPress={handleBack}>
+              <Text style={styles.buttonText}>Back</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={handleClick}>
+              <Text style={styles.buttonText}>
+                {finished_sign_up ? "Save" : "Continue"}
+              </Text>
+            </Pressable>
+          </View>
+        )}
+      </View>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
-    namesContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    titleText: {
-        fontFamily: "Dongle-Regular",
-        fontSize: screenHeight * 0.05
-    },
-    promptContainer: {
-        width: screenWidth * 0.75,
-        height: screenHeight * 0.1,
-        backgroundColor: "#FFFFFF",
-        opacity: 0.8,
-        margin: screenWidth * 0.025,
-        borderRadius: screenWidth * 0.05,
-        shadowColor: '#000000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    promptText: {
-        fontFamily: "Dongle-Regular",
-        fontSize: screenHeight * 0.03,
-        flex: 1,
-        paddingLeft: screenWidth * 0.05,
-    },
-    refresh: {
-        marginRight: screenWidth * 0.025,
-        marginLeft: screenWidth * 0.025,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    refreshText: {
-        size: screenWidth * 0.1,
-        resizeMode: "contain",
-        color: '#77678C',
-        fontFamily: "Dongle-Regular"
-    },
-    inputContainer: {
-        width: screenWidth * 0.75,
-        height: screenHeight * 0.15,
-        backgroundColor: "#FFFFFF",
-        opacity: 0.8,
-        margin: screenWidth * 0.025,
-        borderRadius: screenWidth * 0.05,
-        shadowColor: '#000000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        flexDirection: "column"
-    },
-    input: {   
-        fontFamily: "Dongle-Light",
-        fontSize: screenHeight * 0.03,
-        lineHeight: screenHeight * 0.03,
-        flex: 1,
-        padding: screenWidth * 0.05,
-    },
-    charactersLeftContainer: {
-        flexDirection: "row-reverse"
-    },
-    charactersLeft: {
-        fontFamily: "Dongle-Light",
-        fontSize: screenHeight * 0.02,
-        color: '#888888',
-        paddingRight: screenWidth * 0.025
-    },
-    lowerContainer: {
-        height: screenHeight * 0.05,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: screenHeight * 0.025
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    backButton: {
-        width: screenWidth * 0.3,
-        height: screenHeight * 0.05,
-        backgroundColor: "#77678C",
-        borderRadius: screenWidth * 0.05,
-        alignItems: "center",
-        justifyContent: "center",
-        marginRight: screenWidth * 0.025
-    },
-    button: {
-        width: screenWidth * 0.3,
-        height: screenHeight * 0.05,
-        backgroundColor: "#77678C",
-        borderRadius: screenWidth * 0.05,
-        alignItems: "center",
-        justifyContent: "center",
-        marginLeft: screenWidth * 0.025
-    },
-    buttonText: {
-        color: "#FFFFFF",
-        fontFamily: "Dongle-Light",
-        fontSize: screenHeight * 0.04
-    },
-    loading: {
-        size: "large",
-        justifyContent: "center",
-        alignItems: "center"
-    }
+  namesContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    fontFamily: "Dongle-Regular",
+    fontSize: screenHeight * 0.05,
+  },
+  promptContainer: {
+    width: screenWidth * 0.75,
+    height: screenHeight * 0.1,
+    backgroundColor: "#FFFFFF",
+    opacity: 0.8,
+    margin: screenWidth * 0.025,
+    borderRadius: screenWidth * 0.05,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  promptText: {
+    fontFamily: "Dongle-Regular",
+    fontSize: screenHeight * 0.03,
+    flex: 1,
+    paddingLeft: screenWidth * 0.05,
+  },
+  refresh: {
+    marginRight: screenWidth * 0.025,
+    marginLeft: screenWidth * 0.025,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  refreshText: {
+    size: screenWidth * 0.1,
+    resizeMode: "contain",
+    color: "#77678C",
+    fontFamily: "Dongle-Regular",
+  },
+  inputContainer: {
+    width: screenWidth * 0.75,
+    height: screenHeight * 0.15,
+    backgroundColor: "#FFFFFF",
+    opacity: 0.8,
+    margin: screenWidth * 0.025,
+    borderRadius: screenWidth * 0.05,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    flexDirection: "column",
+  },
+  input: {
+    fontFamily: "Dongle-Light",
+    fontSize: screenHeight * 0.03,
+    lineHeight: screenHeight * 0.03,
+    flex: 1,
+    padding: screenWidth * 0.05,
+  },
+  charactersLeftContainer: {
+    flexDirection: "row-reverse",
+  },
+  charactersLeft: {
+    fontFamily: "Dongle-Light",
+    fontSize: screenHeight * 0.02,
+    color: "#888888",
+    paddingRight: screenWidth * 0.025,
+  },
+  lowerContainer: {
+    height: screenHeight * 0.05,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: screenHeight * 0.025,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backButton: {
+    width: screenWidth * 0.3,
+    height: screenHeight * 0.05,
+    backgroundColor: "#77678C",
+    borderRadius: screenWidth * 0.05,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: screenWidth * 0.025,
+  },
+  button: {
+    width: screenWidth * 0.3,
+    height: screenHeight * 0.05,
+    backgroundColor: "#77678C",
+    borderRadius: screenWidth * 0.05,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: screenWidth * 0.025,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontFamily: "Dongle-Light",
+    fontSize: screenHeight * 0.04,
+  },
+  loading: {
+    size: "large",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default UserPrompt1;
