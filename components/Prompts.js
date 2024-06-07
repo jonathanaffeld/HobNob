a23a9504ca7a2f7c27301517835324683681d8d9
Jonathan Affeld <118568020+jonathanaffeld@users.noreply.github.com>
2024-06-07 16:29:05 -0700

    Create Prompts.js

---
+++ components/Prompts.js
@@ -0,0 +1,24 @@
+const Prompts = [
+    "A guilty pleasure of mine is...",
+    "A life-changing event I experienced was...",
+    "A local spot I love visiting is...",
+    "A moment in history I wish I could have witnessed is...",
+    "A movie/show I can watch over and over is...",
+    "A new skill I want to learn is...",
+    "A subject I could talk about for hours is...",
+    "An invention that I think is underrated is...",
+    "An unusual fear I have is...",
+    "An unusual skill I possess is...",
+    "My favorite annual event/holiday is...",
+    "My favorite childhood memory is...",
+    "My go-to song for a road trip is...",
+    "One place I want to explore is...",
+    "One thing I cannot live without is...",
+    "The best live performance I have ever seen was...",
+    "The last hobby I picked up was...",
+    "The most spontaneous thing I have ever done is...",
+    "The strangest job I ever had was...",
+    "The weirdest food combination I enjoy is..."
+];
+
+export default Prompts;
